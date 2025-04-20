5dda9c9e7f9c2d9ad3ae8c980299f06319f8bf38
swetasachan777 <swetasachan777@gmail.com>
2025-04-21 00:04:54 +0530

    App file updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import Home from './pages/Home'
+import { ToastBar, Toaster } from 'react-hot-toast'
+
+
 
 const App = () => {
+
+  const isSellerPath =useLocation().pathname.includes("seller");
   return (
     <div>
-      <Navbar/>
+      {isSellerPath? null:<Navbar/>}
+      <Toaster/>
+      <div className={`${isSellerPath? "":" px-6 md:px-16 lg:px-14 xl:px-32"}`}>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+        </Routes>
+      </div>
+      
+      
     </div>
   )
 }
